43ede597756f50dcb64ad524459a3a6e1b9c82ad
Vedantgad2509 <vedantgad25@gmail.com>
2023-08-01 13:29:54 +0530

    v d x

---
+++ managing_comm_list/db_connect.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+    //database creation
+    $servername = "localhost";
+    $dusername = "root";
+    $dpassword = "";
+    $dbname = "sgaa";
+
+    $conn = mysqli_connect($servername,$dusername,$dpassword,$dbname);
+    // $conn = new mysqli('localhost','root','trashco');
+
+
+
+?>
