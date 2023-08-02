597b31d42b7678dd0ad575a0e271c84e5e0c3ad4
vedkerkar10 <vedkerkar10@gmail.com>
2023-08-03 00:45:55 +0530

    Update mem_list.php

--- member_lsit/mem_list.php
+++ member_lsit/mem_list.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>showing data</title>
+    <title>South Goa Advocates Association</title>
 </head>
 <body>
     
