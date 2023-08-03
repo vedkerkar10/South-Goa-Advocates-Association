89b61b4e554fd5b862cf51831c422e382e056acf
vedkerkar10 <vedkerkar10@gmail.com>
2023-08-03 23:49:11 +0530

    try

--- member_lsit/mem_list.php
+++ member_lsit/mem_list.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>South Goa Advocates Association</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../src/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@300&display=swap" rel="stylesheet">
@@ -56,6 +56,25 @@
         <h3>103 | GOA | 94</h3>
     </header>
     <main>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../src/index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
         <section class="table-section ">
             <h1 class="text-center">Members List</h1>
             <div class="container table-container">
