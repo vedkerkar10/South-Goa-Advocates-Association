6b91818655001a18ba174c584cfd10d8c168896e
vedkerkar10 <vedkerkar10@gmail.com>
2023-08-03 23:57:25 +0530

    done

--- managing_comm_list/man_com.php
+++ managing_comm_list/man_com.php
@@ -39,6 +39,25 @@
         <h3>103 | GOA | 94</h3>
     </header>
     <main>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../src/index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
         <section class="table-section">
             <h2 class="text-center">Managing Committee List</h2>
             <div class="table-container">
